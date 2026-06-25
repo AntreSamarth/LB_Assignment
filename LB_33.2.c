f706a5c6f398c2f1631aacff67a7ce15471a6aba
Samarth Antre <antresamarth744@gmail.com>
2026-06-25 23:05:26 +0530

    Add files via upload

---
+++ LB_33.2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int FirstChar(char *str, char ch)
+{
+    int iIndex = 0;
+
+    while(*str != '\0')
+    {
+        if(*str == ch)
+        {
+            return iIndex;
+        }
+        iIndex++;
+        str++;
+    }
+    return -1;
+}
+
+int main()
+{
+    char arr[20];
+    char cValue = '\0';
+    int iRet = 0;
+
+    printf("Enter string : ");
+    scanf("%[^\n]s", arr);
+
+    printf("Enter the character : ");
+    scanf(" %c", &cValue);
+
+    iRet = FirstChar(arr, cValue);
+
+    printf("Character location is %d\n", iRet);
+
+    return 0;
+}
