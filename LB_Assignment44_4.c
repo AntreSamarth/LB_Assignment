7f1c39d5d2dd90dd00eea417b5788369cdefb4b8
Samarth Antre <antresamarth744@gmail.com>
2026-07-27 23:34:01 +0530

    Add files via upload

---
+++ LB_Assignment44_4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    static char ch = 'A';
+    if(iNo > 0)
+    {
+        printf("%c\t",ch);
+        ch++;
+        Display(iNo - 1);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
